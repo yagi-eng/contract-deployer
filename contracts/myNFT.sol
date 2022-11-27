31ffd3212648a84185cb7291eb6f3e9fdecf65bb
kohei.yaginuma <f.kohei.yaginuma@gmail.com>
2022-11-27 17:10:17 +0900

    remove `ERC721URIStorage`

--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract myNFT is ERC721URIStorage, Ownable {
+// assume that there are files which name is n (n = 1,2, ..., 100)
+contract myNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -21,7 +22,6 @@ contract myNFT is ERC721URIStorage, Ownable {
 
         uint256 newTokenId = _tokenIds.current();
         _safeMint(recipient, newTokenId);
-        _setTokenURI(newTokenId, "");
 
         return newTokenId;
     }
